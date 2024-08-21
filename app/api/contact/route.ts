512c993337df9e51336eedd6e326881b9a3f3ce3
Graff Alexandre <alexgraff67@gmail.com>
2024-08-21 12:06:11 +0200

    mise en place de la récupération des messages par email

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -20,6 +20,13 @@ export async function POST(request: Request) {
 			text: `Bonjour ${firstName} ${lastName},\n\nMerci de m'avoir contacté. Je vous confirme que j'ai bien reçu votre message et je l'ai pris en compte.\n\nJe vais le traiter avec la plus grande attention et je reviendrai vers vous dans les meilleurs délais.\n\nN'hésitez pas à me recontacter si vous avez d'autres questions ou si vous souhaitez ajouter des informations complémentaires.\n\nCordialement,\n\nAlexandre`,
 		});
 
+		await transporter.sendMail({
+			from: process.env.GMAIL_USER,
+			to: 'alexgraff67@gmail.com',
+			subject: 'Nouveau message de contact via votre portfolio',
+			text: `Vous avez reçu un nouveau message de contact via votre portfolio.\n\nNom : ${firstName} ${lastName}\n\nEmail : ${email}\n\nMessage :\n\n${message}\n\nVeuillez répondre à cet email dans les meilleurs délais pour assurer une bonne communication.`,
+		});
+
 		return NextResponse.json({ message: 'Email envoyé avec succès' });
 	} catch (error) {
 		console.error("Erreur lors de l'envoi de l'email:", error);
